4eddc1dd18884730e23f4356572d376d6d518a31
9551-Dev <9551dev@gmail.com>
2023-08-10 18:59:59 +0200

    add loading cursor

--- loading_text.js
+++ loading_text.js
@@ -1,29 +1,34 @@
 const texts = ["CCAA", "CCAA LLC", "CCAA Team", "Communist Christians Against Alzheimers", "The ones feared."];
-  const loadingText = document.getElementById('desc');
+const loadingText = document.getElementById('desc');
+const loadingCursor = document.getElementById('load-cursor');
 
-  function random(min, max) {
-      return Math.floor(Math.random() * (max - min) + min);
-  }
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
-  function typeWriter(i = random(0, texts.length), index = 0) {
-      if (index <= texts[i].length) {
-          loadingText.textContent = texts[i].substr(0, index);
-          setTimeout(() => typeWriter(i, index + 1), 100);
-      } else {
-          setTimeout(() => {
-              loadingText.style.display = "none";
-              setTimeout(() => {
-                  loadingText.innerHTML = texts[i].split('').map(char => {
-                      return `<span class="glitch">${char}</span>`;
-                  }).join('');
+function typeWriter(i = random(0, texts.length), index = 0) {
+    if (index <= texts[i].length) {
+        loadingText.textContent = "> " + texts[i].substr(0, index);
+        loadingCursor.style.display = 'inline';
+        setTimeout(() => {
+            typeWriter(i, index + 1);
+        }, 1000 / texts[i].length);
+    } else {
+        setTimeout(() => {
+            loadingText.style.display = "none";
+            loadingCursor.style.display = 'none';
+            setTimeout(() => {
+                loadingText.innerHTML = texts[i].split('').map(char => {
+                    return `<span class="glitch">${char}</span>`;
+                }).join('');
 
-                  document.querySelector('.container').style.opacity = 1;
-                  document.querySelector('.content').style.display = "block";
-                  canvas.style.display = "block";
-                  setInterval(draw, 35);
-              }, 200);
-          }, 200);
-      }
-  }
+                document.querySelector('.container').style.opacity = 1;
+                document.querySelector('.content').style.display = "block";
+                canvas.style.display = "block";
+                setInterval(draw, 35);
+            }, 200);
+        }, 200);
+    }
+}
 
-  typeWriter();
+typeWriter();
